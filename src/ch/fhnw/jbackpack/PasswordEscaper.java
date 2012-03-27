93440feeeccb415df0754a1f1dd11b7ee821f52d
ronnystandtke <ronnystandtke@abd854b9-26a2-4ff2-a482-16932eac8f7f>
2012-03-27 20:58:12 +0000

    small code optimization

--- src/ch/fhnw/jbackpack/PasswordEscaper.java
+++ src/ch/fhnw/jbackpack/PasswordEscaper.java
@@ -1,3 +1,25 @@
+/**
+ * PasswordEscaper.java
+ *
+ * Copyright (C) 2010 imedias
+ *
+ * This file is part of JBackpack.
+ *
+ * JBackpack is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * JBackpack is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created on 27. March 2012, 22:31
+ */
 package ch.fhnw.jbackpack;
 
 /**
@@ -14,10 +36,9 @@ public class PasswordEscaper {
      * @return escaped password
      */
     public static String escapePassword(char[] password) {
-        String input = "";
-        for (char c : password) {
-            input += c;
-        }
+        StringBuilder stringBuilder = new StringBuilder(password.length);
+        stringBuilder.append(password);
+        String input = stringBuilder.toString();
         String escapedInput = input.replace("\"", "\\\"");
         escapedInput = escapedInput.replace("`", "\\`");
         escapedInput = escapedInput.replace("$", "\\$");
