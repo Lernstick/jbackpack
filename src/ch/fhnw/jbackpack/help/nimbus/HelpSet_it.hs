7527afa258f7ccf23d276637640c8be4c8ecae9b
ronnystandtke <ronnystandtke@abd854b9-26a2-4ff2-a482-16932eac8f7f>
2012-04-22 19:20:16 +0000

    added Italian HelpSets

---
+++ src/ch/fhnw/jbackpack/help/nimbus/HelpSet_it.hs
@@ -0,0 +1,37 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+<helpset xml:lang=it version="1.0">
+   <!-- title -->
+   <title>JBackpack-Hilfe</title>
+
+   <!-- maps -->
+   <maps>
+     <homeID>Index</homeID>
+     <mapref location="it/jhelpmap.jhm" />
+   </maps>
+
+   <!-- views -->
+   <view>
+      <name>TOC</name>
+      <label>Table Of Contents</label>
+      <type>javax.help.TOCView</type>
+      <data>it/jhelptoc.xml</data>
+   </view>
+   <view>
+      <name>Index</name>
+      <label>Index</label>
+      <type>javax.help.IndexView</type>
+      <data>it/jhelpidx.xml</data>
+   </view>
+   <view>
+      <name>Search</name>
+      <label>Search</label>
+      <type>javax.help.SearchView</type>
+      <data engine="com.sun.java.help.search.DefaultSearchEngine">
+         JavaHelpSearch_it
+      </data>
+   </view>
+
+</helpset>
